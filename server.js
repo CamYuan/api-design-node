53972d5a7b4b96771ec3c06f7e9740d544b99eca
Cameron Yuan <cameronmyuan@gmail.com>
2018-08-01 11:16:53 -0500

    Complete Excercise

--- server.js
+++ server.js
@@ -2,4 +2,25 @@
 // that will send back the index.html file on a GET request to '/'
 // it should then send back jsonData on a GET to /data
 
+var express = require('express');
+var app = express();
+
 var jsonData = {count: 12, message: 'hey'};
+
+app.get('/', function(req, res){
+  //res.sendFile take an absolute path to a file and sets the mime type based on the file extname
+  res.sendFile(__dirname + '/index.html', function(err){
+    if (err) {
+      res.status(500).send(err);
+    }
+  })
+});
+
+app.get('/data', function(req, res){
+  res.json(jsonData);
+});
+
+var port = 3000;
+app.listen(port, function(){
+  console.log('Listening on port: ', port)
+});
