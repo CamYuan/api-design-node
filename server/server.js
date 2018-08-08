79ad0aa52d11485350d5c65680be31d981f388cc
Cameron Yuan <cameronmyuan@gmail.com>
2018-08-08 11:31:07 -0500

    Added generic error handle

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var app = express();
 var api = require('./api');
+var err = require('./middleware/error')
 
 // setup the app middlware
 require('./middleware')(app);
@@ -8,6 +9,7 @@ require('./middleware')(app);
 // setup the api
 app.use('/api/', api);
 // set up global error handling
+app.use(err())
 
 // export the app for testing
 module.exports = app;
